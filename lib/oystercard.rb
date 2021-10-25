287940df8a61904fb66c933911e66d53ce2b6fcb
Mark McLeod <markmcleod45@gmail.com>
2021-10-25 15:36:09 +0100

    Add tests for top_up method and the code to pass them

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,9 +1,11 @@
-class Oystercard 
+class Oystercard
   attr_reader :balance
 
   def initialize
     @balance = 0
   end
 
-
+  def top_up(amount)
+    @balance = @balance + amount
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,8 +2,13 @@ require './lib/oystercard'
 
 describe Oystercard do
   let(:oystercard) { Oystercard.new }
-  it "expects there to be a balance on the card" do
+  it 'expects there to be a balance on the card' do
     expect(oystercard.balance).to eq(0)
   end
 
+  it { is_expected.to respond_to(:top_up).with(1).argument }
+
+  it 'expects to be able to add money to a card' do
+    expect { oystercard.top_up 10 }.to change { oystercard.balance }.by(10)
+  end
 end
