074ddc2309f9ca2475a6b90dc7f31454c402e902
Mohammed Salah <mohammed_salah@icloud.com>
2021-10-25 16:09:52 +0100

    Added a maximum balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,4 +1,5 @@
 class Oystercard
+  MAXIMUM_BALANCE = 90
   attr_reader :balance
 
   def initialize
@@ -6,6 +7,7 @@ class Oystercard
   end
 
   def top_up(amount)
+    fail "Max. balance exceeded" if (@balance + amount) > MAXIMUM_BALANCE
     @balance = @balance + amount
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,13 +2,24 @@ require './lib/oystercard'
 
 describe Oystercard do
   let(:oystercard) { Oystercard.new }
-  it 'expects there to be a balance on the card' do
-    expect(oystercard.balance).to eq(0)
+
+  describe "#balance" do
+    it 'expects there to be a balance on the card' do
+      expect(oystercard.balance).to eq(0)
+    end
   end
 
-  it { is_expected.to respond_to(:top_up).with(1).argument }
+  describe "#top_up" do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
 
-  it 'expects to be able to add money to a card' do
-    expect { oystercard.top_up 10 }.to change { oystercard.balance }.by(10)
+    it 'expects to be able to add money to a card' do
+      expect { oystercard.top_up 10 }.to change { oystercard.balance }.by(10)
+    end
+
+    it 'expects to raise an error if balance exceeds 90' do
+      maximum_balance = Oystercard::MAXIMUM_BALANCE
+      oystercard.top_up(maximum_balance)
+      expect { oystercard.top_up 1 }.to raise_error "Max. balance exceeded"
+    end
   end
 end
